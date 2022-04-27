1ad683f18f06d1f3eae8976aaafa6e12ae55ca06
alexandre114 <alexandre@akufen.ca>
2022-04-27 19:12:17 -0400

    add svgv to style guide

---
+++ components/shared/SvgIcon/svgList.js
@@ -0,0 +1,3 @@
+const svgList = ['logo', 'menu', 'close'];
+
+export default svgList;
--- pages/styleGuide.vue
+++ pages/styleGuide.vue
@@ -2,8 +2,8 @@
 	<SmoothScroll>
 		<div class="styleguide">
 			<Hero title="StyleGuide" />
-			<div class="section typography">
-				<h2 class="title">Typography</h2>
+			<div class="section section--typography">
+				<h2 class="section__title">Typography</h2>
 				<h1 class="uber-style">Uber Style</h1>
 				<h1 class="h1-style">H1 Style</h1>
 				<h2 class="h2-style">H2 Style</h2>
@@ -15,19 +15,34 @@
 				<p class="body-m-style">Body-m Style</p>
 				<p class="body-s-style">Body-s Style</p>
 			</div>
-			<div class="section spacers">
-				<h2 class="title">Spacers</h2>
-				<br />
-				<div class="spacer-xxs">xxs</div>
-				<div class="spacer-xs">xs</div>
-				<div class="spacer-s">s</div>
-				<div class="spacer-m">m</div>
-				<div class="spacer-l">l</div>
-				<div class="spacer-xl">xl</div>
-				<div class="spacer-xxl">xxl</div>
+			<div class="section section--spacers">
+				<h2 class="section__title">Spacers</h2>
+				<div class="spacer spacer-xxs">xxs</div>
+				<div class="spacer spacer-xs">xs</div>
+				<div class="spacer spacer-s">s</div>
+				<div class="spacer spacer-m">m</div>
+				<div class="spacer spacer-l">l</div>
+				<div class="spacer spacer-xl">xl</div>
+				<div class="spacer spacer-xxl">xxl</div>
 			</div>
-			<div class="section svgs">
-				<h2 class="title">Svgs</h2>
+			<div class="section section--svgs">
+				<h2 class="section__title">Svgs</h2>
+				<div class="section__container">
+					<SvgIcon v-for="svg in svgList" :key="svg" :name="svg" />
+				</div>
+			</div>
+			<div class="section section--colors">
+				<h2 class="section__title">Colors</h2>
+				<div class="color-main">Color Main ( $color-main )</div>
+				<div class="color-secondary">
+					Color Secondary ( $color-secondary )
+				</div>
+				<div class="color-text-main">
+					Color Text Main ( $color-text-main )
+				</div>
+				<div class="color-text-secondary">
+					Color Text Secondary ( $color-text-secondary )
+				</div>
 			</div>
 		</div>
 	</SmoothScroll>
@@ -36,11 +51,19 @@
 <script>
 import SmoothScroll from '@/components/core/SmoothScroll.vue';
 import Hero from '@/components/sections/Hero.vue';
+import SvgIcon from '@/components/shared/SvgIcon/SvgIcon.vue';
+import svgList from '@/components/shared/SvgIcon/svgList';
 export default {
 	name: 'Home',
 	components: {
 		SmoothScroll,
 		Hero,
+		SvgIcon,
+	},
+	data() {
+		return {
+			svgList: svgList,
+		};
 	},
 };
 </script>
@@ -48,64 +71,156 @@ export default {
 <style lang="scss">
 .styleguide {
 	width: 100%;
-	background-color: #000;
-	padding: 100px 100px 400px;
-
-	.title {
-		padding: $xs;
-		background-color: #fff;
-		color: #000;
-		@extend .h4-style;
-		margin-bottom: $xxs;
-	}
+	background-color: #fff;
+	padding: 0 100px 100px 100px;
+	color: #000;
 
 	.section {
 		margin-bottom: $m;
-	}
 
-	.spacers {
-		margin-top: $s;
-		div {
-			margin-bottom: 10px;
-			display: flex;
-			align-items: center;
-			justify-content: center;
+		&__container {
+			padding: 0 $xs $xs $xs;
+		}
+
+		&__title {
+			@extend .h4-style;
+			padding: $xs;
+			background-color: #000;
+			color: #fff;
+			margin-bottom: $xs;
+		}
+
+		&--svgs {
+			background-color: gray;
+
+			svg {
+				width: 48px;
+				height: 48px;
+				fill: #fff;
+				margin-right: $xs;
+			}
+		}
+
+		&--spacers {
+			margin-top: $s;
+
+			div {
+				margin-bottom: 10px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+		}
+
+		&--colors {
+			.color-main {
+				display: flex;
+				align-items: center;
+				margin-bottom: $xxs;
+				line-height: 1em;
+				&:before {
+					content: '';
+					display: block;
+					width: 1em;
+					height: 1em;
+					background-color: $color-main;
+					display: inline-block;
+					border-radius: 4px;
+					margin-right: 5px;
+					border: 1px solid gray;
+				}
+			}
+			.color-secondary {
+				display: flex;
+				align-items: center;
+				margin-bottom: $xxs;
+				line-height: 1em;
+				&:before {
+					content: '';
+					display: block;
+					width: 1em;
+					height: 1em;
+					background-color: $color-secondary;
+					display: inline-block;
+					border-radius: 4px;
+					margin-right: 5px;
+					border: 1px solid gray;
+				}
+			}
+			.color-text-main {
+				display: flex;
+				align-items: center;
+				margin-bottom: $xxs;
+				line-height: 1em;
+				&:before {
+					content: '';
+					display: block;
+					width: 1em;
+					height: 1em;
+					background-color: $color-text-main;
+					display: inline-block;
+					border-radius: 4px;
+					margin-right: 5px;
+					border: 1px solid gray;
+				}
+			}
+			.color-text-secondary {
+				display: flex;
+				align-items: center;
+				margin-bottom: $xxs;
+				line-height: 1em;
+				&:before {
+					content: '';
+					display: block;
+					width: 1em;
+					height: 1em;
+					background-color: $color-text-secondary;
+					display: inline-block;
+					border-radius: 4px;
+					margin-right: 5px;
+					border: 1px solid gray;
+				}
+			}
 		}
 	}
-	.spacer-xxs {
-		width: $xxs;
-		height: $xxs;
-		background-color: gray;
-	}
-	.spacer-xs {
-		width: $xs;
-		height: $xs;
-		background-color: gray;
-	}
-	.spacer-s {
-		width: $s;
-		height: $s;
-		background-color: gray;
-	}
-	.spacer-m {
-		width: $m;
-		height: $m;
-		background-color: gray;
-	}
-	.spacer-l {
-		width: $l;
-		height: $l;
-		background-color: gray;
-	}
-	.spacer-xl {
-		width: $xl;
-		height: $xl;
-		background-color: gray;
-	}
-	.spacer-xxl {
-		width: $xxl;
-		height: $xxl;
-		background-color: gray;
+
+	.spacer {
+		background-color: lightgray;
+
+		&-xxs {
+			width: $xxs;
+			height: $xxs;
+		}
+
+		&-xs {
+			width: $xs;
+			height: $xs;
+		}
+
+		&-s {
+			width: $s;
+			height: $s;
+		}
+
+		&-m {
+			width: $m;
+			height: $m;
+		}
+
+		&-l {
+			width: $l;
+			height: $l;
+		}
+
+		&-xl {
+			width: $xl;
+			height: $xl;
+		}
+
+		&-xxl {
+			width: $xxl;
+			height: $xxl;
+		}
 	}
 }
 </style>
